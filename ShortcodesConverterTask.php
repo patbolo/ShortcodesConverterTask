6b2448098e4bd5bd2e85f414f4a434698d0f3a27
jean <jfbarrois@gmail.com>
2013-08-02 11:37:16 +1200

    MINOR Also converts when there's a space instead of %20

--- ShortcodesConverterTask.php
+++ ShortcodesConverterTask.php
@@ -17,13 +17,21 @@ class ShortcodesConverterTask extends BuildTask {
 				foreach($fields as $fieldName=>$fieldType){
 					if (in_array($fieldType, $htmlTextClasses)){
 						echo "Field ".$fieldName." is of type HTMLText (or descendant) and is now converted\n";
+						
 						DB::query("UPDATE \"$dataClass\" SET \"$fieldName\" = REPLACE(\"$fieldName\", '[sitetree_link%20', '[sitetree_link,')"); 
+						DB::query("UPDATE \"$dataClass\" SET \"$fieldName\" = REPLACE(\"$fieldName\", '[sitetree_link ', '[sitetree_link,')");
 						DB::query("UPDATE \"$dataClass\" SET \"$fieldName\" = REPLACE(\"$fieldName\", '[document_link%20', '[document_link,')"); 
+						DB::query("UPDATE \"$dataClass\" SET \"$fieldName\" = REPLACE(\"$fieldName\", '[document_link ', '[document_link,')"); 
 						if (Object::has_extension($dataClass, 'Versioned')){
+							echo "Class ".$dataClass." is versioned; converting live and versioned records too\n";
 							DB::query("UPDATE \"{$dataClass}_Live\" SET \"$fieldName\" = REPLACE(\"$fieldName\", '[sitetree_link%20', '[sitetree_link,')"); 
+							DB::query("UPDATE \"{$dataClass}_Live\" SET \"$fieldName\" = REPLACE(\"$fieldName\", '[sitetree_link ', '[sitetree_link,')"); 
 							DB::query("UPDATE \"{$dataClass}_Live\" SET \"$fieldName\" = REPLACE(\"$fieldName\", '[document_link%20', '[document_link,')"); 
+							DB::query("UPDATE \"{$dataClass}_Live\" SET \"$fieldName\" = REPLACE(\"$fieldName\", '[document_link ', '[document_link,')");
 							DB::query("UPDATE \"{$dataClass}_versions\" SET \"$fieldName\" = REPLACE(\"$fieldName\", '[sitetree_link%20', '[sitetree_link,')"); 
+							DB::query("UPDATE \"{$dataClass}_versions\" SET \"$fieldName\" = REPLACE(\"$fieldName\", '[sitetree_link ', '[sitetree_link,')");
 							DB::query("UPDATE \"{$dataClass}_versions\" SET \"$fieldName\" = REPLACE(\"$fieldName\", '[document_link%20', '[document_link,')"); 
+							DB::query("UPDATE \"{$dataClass}_versions\" SET \"$fieldName\" = REPLACE(\"$fieldName\", '[document_link ', '[document_link,')"); 
 						}
 					}
 				}
