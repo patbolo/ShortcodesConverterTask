f1681768ce43862af508ef5015c2de8d0cd050eb
jean <jfbarrois@gmail.com>
2013-08-02 10:31:30 +1200

    First version of the task

---
+++ ShortcodesConverterTask.php
@@ -0,0 +1,33 @@
+<?php
+
+
+class ShortcodesConverterTask extends BuildTask {
+	
+	public function run($request) {
+		if (!(DB::getConn() instanceof MySQLDatabase)){
+			user_error('Sorry this task only runs on Mysql for the moment');
+		}
+		if (Director::is_cli() || Permission::check('ADMIN')){
+			$dataClasses = ClassInfo::subclassesFor('DataObject');
+			$htmlTextClasses = ClassInfo::subclassesFor('HTMLText');
+			array_shift($dataClasses);
+			foreach($dataClasses as $dataClass){
+				$fields = DataObject::custom_database_fields($dataClass);
+				echo "\n Inspecting class ".$dataClass."\n";
+				foreach($fields as $fieldName=>$fieldType){
+					if (in_array($fieldType, $htmlTextClasses)){
+						echo "Field ".$fieldName." is of type HTMLText (or descendant) and is now converted\n";
+						DB::query("UPDATE \"$dataClass\" SET \"$fieldName\" = REPLACE(\"$fieldName\", '[sitetree_link%20', '[sitetree_link,')"); 
+						DB::query("UPDATE \"$dataClass\" SET \"$fieldName\" = REPLACE(\"$fieldName\", '[document_link%20', '[document_link,')"); 
+						if (Object::has_extension($dataClass, 'Versioned')){
+							DB::query("UPDATE \"{$dataClass}_Live\" SET \"$fieldName\" = REPLACE(\"$fieldName\", '[sitetree_link%20', '[sitetree_link,')"); 
+							DB::query("UPDATE \"{$dataClass}_Live\" SET \"$fieldName\" = REPLACE(\"$fieldName\", '[document_link%20', '[document_link,')"); 
+							DB::query("UPDATE \"{$dataClass}_versions\" SET \"$fieldName\" = REPLACE(\"$fieldName\", '[sitetree_link%20', '[sitetree_link,')"); 
+							DB::query("UPDATE \"{$dataClass}_versions\" SET \"$fieldName\" = REPLACE(\"$fieldName\", '[document_link%20', '[document_link,')"); 
+						}
+					}
+				}
+			}
+		}
+	}
+}
